3f5e7acfe3b8d3460a743fec6a6f1499e0d4921a
will14lee <will14lee@gmail.com>
2021-05-27 14:26:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names=([`Lisa`, `Kaitlin`, `Jan`]);
+function writeCards(names) {
+    let message= [];
+    for(let i = 0; i < names.length; i++){
+     message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    console.log(message);
+    return message;
+    }
+
+function countDown (positiveNumber){
+    while(positiveNumber >= 0){
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+    }
